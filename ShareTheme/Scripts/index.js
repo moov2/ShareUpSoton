e17ef1d6893e5f22625365c40906c9efa2a53e44
Jack-Moov2 <jack@moov2.com>
2016-06-23 10:04:52 +0100

    Creation of Orchard Theme for ShareUp

---
+++ ShareTheme/Scripts/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * File acts as the input for browserify, which will handle compiling
+ * modules into a single file. It shouldn't be necessary to add JS code
+ * to this file, instead `app/app.js` should be used as the starting point
+ * for JavaScript code.
+ */
+
+require('shoestring');
+
+var app = require('app')(),
+	browser = require('browser');
+
+/**
+ * Setup different layers of the application.
+ */
+
+app.addLayer(browser);
---
+++ ShareTheme/Scripts/tests/suite.js
@@ -0,0 +1,8 @@
+"use strict";
+
+require('shoestring');
+
+require('app.tests');
+
+require('browser.tests');
+require('browser/set-classes.tests');
